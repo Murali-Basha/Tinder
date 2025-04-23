be0808f47e10ca35b9f971f5df888e290829603d
Murali-Basha <Mbasha@trcsolutions.com>
2025-04-23 15:07:26 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.post("/user", (req, res) => {
-  res.send("User data saved successfully!");
-});
-// this will match only get api call
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ firstname: "Murali", lastname: "Basha" });
-});
-// this will match all the http api calls to test
-app.use("/test", (req, res) => {
-  res.send("Welcome to the homepage");
-});
-app.delete("/user", (req, res) => {
-  res.send("User deleted!");
 });
 
 app.listen(3000, () => {
