db7891a55d5f321fbcd1a02f1b7b19c0ff645bf6
Murali-Basha <Mbasha@trcsolutions.com>
2025-04-23 12:39:35 +0530

    exploring get,post,delete methods

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.post("/user", (req, res) => {
+  res.send("User data saved successfully!");
+});
+// this will match only get api call
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Murali", lastname: "Basha" });
+});
+// this will match all the http api calls to test
 app.use("/test", (req, res) => {
   res.send("Welcome to the homepage");
 });
-app.use("/", (req, res) => {
-  res.send("Hello from the Server!");
+app.delete("/user", (req, res) => {
+  res.send("User deleted!");
 });
+
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000`);
 });
